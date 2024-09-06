9d8bd019d583406272793ae25eb7537cafe8e23f
ayimdomnic <ayimdomnic@outlook.com>
2024-09-06 18:19:47 +0300

    chore(tanda-client): refactor the base client

--- src/client/tanda.ts
+++ src/client/tanda.ts
@@ -5,43 +5,89 @@ import { BaseUrls } from "../config";
 import axios from "axios";
 import { BaseTandaException } from "../exceptions";
 import type { AccessTokenResponse } from "../types/access_token";
+import type { BaseURL } from "../types";
 
+/**
+ * The TandaClient class is responsible for interacting with the Tanda API.
+ * It handles authentication, token management, and API request logic.
+ */
 export class TandaClient {
+  /**
+   * Axios instance used to make HTTP requests.
+   * @private
+   */
   private client: AxiosInstance;
+
+  /**
+   * Configuration object containing client credentials and environment details.
+   * @private
+   */
   private config: IConfig;
-  private baseUrl = BaseUrls;
-  private accessToken: string | undefined | null;
 
+  /**
+   * Object containing base URLs for different environments (UAT, Production, etc.).
+   * @private
+   * @type { BaseURL }
+   */
+  private baseUrl: BaseURL = BaseUrls;
+
+  /**
+   * Access token used for authenticating API requests.
+   * This is obtained using the OAuth 2.0 Client Credentials Flow.
+   * @private
+   */
+  private accessToken?: string | null;
+
+  /**
+   * Creates an instance of TandaClient.
+   * Initializes the client with a configuration, and fetches an access token.
+   *
+   * @param {Partial<IConfig>} config - Partial configuration object to customize the client's behavior.
+   */
   constructor(config: Partial<IConfig>) {
-    const defaultConfig: IConfig = {
-      mode: "uat",
-      clientId: String(process.env.TANDA_CLIENT_ID),
-      clientSecret: String(process.env.TANDA_CLIENT_SECRET),
-      debug: process.env.NODE_ENV === "development",
-    };
+    const defaultConfig: IConfig = this.getDefaultConfig(config);
+    this.config = ConfigSchema.parse(defaultConfig);
 
-    this.config = ConfigSchema.parse({ ...config, ...defaultConfig });
-
-    const options: AxiosRequestConfig = {
+    // Initialize Axios client with default settings
+    this.client = axios.create({
       baseURL: this.baseUrl[this.config.mode],
       validateStatus: (status) => status >= 200 && status <= 500,
-    };
+    });
 
-    this.client = axios.create(options);
-
+    // Automatically generate access token on initialization
     this.generateAccessToken();
   }
 
   /**
-   * This method generates the access token from the Tanda Api
-   * The aim of this is to have the client always connected and ready for requests,
-   * Each api resource or operation api will need to extend this class
-   * Todo::add optional caching so the user can always have the token state and reduce number of calls
-   * @returns { Promise<void> } - an empty promise as the token is assigned to the class property
-   * this can be extended by DI frameworks to ensure token generation and valiation for frameworks like nestjs
+   * Generates the default configuration by merging the user-provided config with default values.
+   *
+   * @param {Partial<IConfig>} config - Partial user configuration.
+   * @returns {IConfig} - Complete configuration with default values.
+   * @private
+   */
+  private getDefaultConfig(config: Partial<IConfig>): IConfig {
+    return {
+      mode: "uat",
+      clientId: String(process.env.TANDA_CLIENT_ID),
+      clientSecret: String(process.env.TANDA_CLIENT_SECRET),
+      debug: process.env.NODE_ENV === "development",
+      ...config,
+    };
+  }
+
+  /**
+   * Generates an OAuth 2.0 access token using the Tanda API.
+   * The generated token is stored in the `accessToken` class property for future requests.
+   *
+   * @remarks
+   * This method should ideally be called during initialization to ensure the client is authenticated.
+   * Future improvements could include caching the token to avoid unnecessary API calls.
+   *
+   * @returns {Promise<void>} - A promise that resolves when the token has been successfully generated.
+   * @private
    */
   private async generateAccessToken(): Promise<void> {
-    const option: AxiosRequestConfig = {
+    const requestOptions: AxiosRequestConfig = {
       auth: {
         username: this.config.clientId,
         password: this.config.clientSecret,
@@ -56,64 +102,72 @@ export class TandaClient {
 
     const response = await this.call<AccessTokenResponse>(
       "accounts/v1/oauth/token",
-      option,
+      requestOptions,
       "POST"
     );
 
     this.accessToken = response.access_token;
-
-    //add to cache or something
   }
 
-/**
- * Get access token using the OAuth 2.0 Client Credentials Flow
- * 
- * @link https://docs.tanda.africa/reference/authentication-and-authorization
- * @param { string } url 
- * @param { AxiosRequestConfig } options - this contains the request options configs, here you can set additional headers as per the docs
- * @param { 'GET' | 'POST' } method - This involves the request methods for making the requests
- * @returns { Promise<T>} Promise<T> - Returns the promise with the valid response type from the call made at the point
- * this also allows for easier use of the sdk for nodejs frameworks that require very little configuration
- * @throws { BaseTandaException } - An exception to represent all statuses from the platform
- */
+  /**
+   * Makes an API call to the Tanda platform.
+   *
+   * @template T - The expected response type.
+   * @param {string} url - The URL endpoint for the API call.
+   * @param {AxiosRequestConfig} [options={}] - Optional Axios request configuration settings.
+   * @param {"POST" | "GET"} [method="POST"] - The HTTP method to use for the request.
+   * @returns {Promise<T>} - A promise resolving with the API response data.
+   *
+   * @throws {BaseTandaException} - Throws an exception when the request fails.
+   *
+   * @remarks
+   * This method automatically attaches the access token in the request headers if available.
+   */
   private async call<T>(
     url: string,
     options: AxiosRequestConfig = {},
     method: "POST" | "GET" = "POST"
   ): Promise<T> {
-    if (this.accessToken) {
-      options.headers = {
+    const requestOptions: AxiosRequestConfig = {
+      ...options,
+      method,
+      url,
+      headers: {
         ...options.headers,
-        Authorization: `Bearer ${this.accessToken}`,
-      };
-    }
+        ...(this.accessToken && {
+          Authorization: `Bearer ${this.accessToken}`,
+        }),
+      },
+    };
 
     try {
-      const response: AxiosResponse<T> = await this.client.request<T>({
-        url,
-        method,
-        ...options.headers,
-      });
-
+      const response: AxiosResponse<T> = await this.client.request<T>(
+        requestOptions
+      );
       return response.data;
     } catch (error: any) {
-      if (error.response) {
-        if (error.response.status >= 500) {
-          throw new BaseTandaException(
-            `Tanda Apis Error: ${error.response.message}`,
-            error.response.status
-          );
-        } else {
-          throw new BaseTandaException(
-            `Tanda Apis Error: ${error.response.message}`,
-            error.response.status
-          );
-        }
-      } else if (error.request) {
-        throw new BaseTandaException("Tanda APIs: Gateway Timeout", 504);
-      } else {
-        throw new BaseTandaException(`Tanda APIs: ${error.message}`, 500);
-      }
+      this.handleError(error);
+    }
+  }
+
+  /**
+   * Handles errors that occur during API requests.
+   *
+   * @param {any} error - The error object from Axios.
+   * @throws {BaseTandaException} - Throws a custom exception based on the error type.
+   *
+   * @private
+   */
+  private handleError(error: any): never {
+    if (error.response) {
+      throw new BaseTandaException(
+        `Tanda APIs Error: ${error.response.message}`,
+        error.response.status
+      );
+    } else if (error.request) {
+      throw new BaseTandaException("Tanda APIs: Gateway Timeout", 504);
+    } else {
+      throw new BaseTandaException(`Tanda APIs: ${error.message}`, 500);
     }
   }
 }
